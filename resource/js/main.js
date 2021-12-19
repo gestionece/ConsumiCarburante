99f3c58b447d14d992ae6c5879e500a975ebc4b9
gestionece <gestionece@gmail.com>
2021-12-19 23:37:01 +0100

    Update main.js

--- resource/js/main.js
+++ resource/js/main.js
@@ -65,98 +65,181 @@ var app = new Vue({
 
                 this.page_CpList_loader = true;
                 this.btnLoad_isDisabled = true;
+
                 const worker = new Worker('resource/js/worker.js'); //https://dog.ceo/dog-api/
-                worker.postMessage(selectedFile);
-                worker.onmessage = (e) => {
+                let nFile = selectedFile.length;
 
-                    let loadFileData = e.data; //uso worker.js per ricevere già JSON dal file EXCEL, problema consite nel riceve due volte, visto che ci sono pagine diverse(si potrebbe valuitare di utlizare un foglio per un contratto).
-                    //console.log(loadFileData);
+                let loadFileData = [];
 
-                    this.CpList = {
-                        TARGA: [],
-                        TOTALE: {
-                            KM_VEI: 0,
-                            L_CIS: 0,
-                            L_VEI: 0
-                        }
-                    };
+                let readFile = (index) => {
+                    if (index >= selectedFile.length) return;
+                    var file = selectedFile[index];
 
-                    this.selectedFile_name = selectedFile.name;
+                    worker.postMessage(file);
 
-                    loadFileData.forEach(row => {
-                        if (this.CpList.TARGA[row.TARGA] == undefined) {
-                            this.CpList.TARGA[row.TARGA] = {
-                                KM: [],
-                                L: [],
-                                IMPORTO: [],
-                                DATA: [],
-                                LOCALITÀ: [],
-                                AUTISTA: [],
-                                L_TOT: 0,
-                                KM_TOT: 0,
-                                IMPORTO_TOT: 0,
-                                BU_KM: row.CHILOMETRAGGIO,
-                                KM_ERROR: false,
+                    worker.onmessage = (e) => {
+
+                        loadFileData = loadFileData.concat(e.data); //uso worker.js per ricevere già JSON dal file EXCEL, problema consite nel riceve due volte, visto che ci sono pagine diverse(si potrebbe valuitare di utlizare un foglio per un contratto).
+
+                        nFile--;
+
+                        if (nFile == 0) {
+
+                            this.CpList = {
+                                TARGA: [],
+                                TOTALE: {
+                                    KM_VEI: 0,
+                                    L_CIS: 0,
+                                    L_VEI: 0,
+
+                                    KM_TOT_M: {
+                                        gennaio: 0,
+                                        febbraio: 0,
+                                        marzo: 0,
+                                        aprile: 0,
+                                        maggio: 0,
+                                        giugno: 0,
+                                        luglio: 0,
+                                        agosto: 0,
+                                        settembre: 0,
+                                        ottobre: 0,
+                                        novembre: 0,
+                                        dicembre: 0,
+                                    }
+                                }
                             };
+
+                            this.selectedFile_name = selectedFile.name;
+
+                            loadFileData.forEach(row => {
+                                if (this.CpList.TARGA[row.TARGA] == undefined) {
+                                    this.CpList.TARGA[row.TARGA] = {
+                                        TRANSAZIONI: [],
+                                        L_TOT: 0,
+                                        KM_TOT: 0,
+                                        IMPORTO_TOT: 0,
+                                        BU_KM: row.CHILOMETRAGGIO,
+                                        KM_ERROR: false,
+
+                                        KM_TOT_M: {
+                                            gennaio: 0,
+                                            febbraio: 0,
+                                            marzo: 0,
+                                            aprile: 0,
+                                            maggio: 0,
+                                            giugno: 0,
+                                            luglio: 0,
+                                            agosto: 0,
+                                            settembre: 0,
+                                            ottobre: 0,
+                                            novembre: 0,
+                                            dicembre: 0,
+                                        }
+                                    };
+                                }
+
+                                if (row["STATO AUTORIZZATIVO"] == "Approvata" &&
+                                    row["PREZZO UNITARIO"] > 1 &&
+                                    row["PREZZO UNITARIO"] < 10 &&
+                                    row.QUANTITÀ != 0) {
+
+                                    this.CpList.TARGA[row.TARGA].L_TOT += row.QUANTITÀ;
+                                    this.CpList.TARGA[row.TARGA].IMPORTO_TOT += row.IMPORTO;
+
+                                    if ((row.TARGA).substring(0, 3) == "CIS") {
+                                        this.CpList.TOTALE.L_CIS += row.QUANTITÀ;
+                                    } else {
+                                        this.CpList.TOTALE.L_VEI += row.QUANTITÀ;
+                                    }
+
+                                    if (row.CHILOMETRAGGIO > 10 && this.CpList.TARGA[row.TARGA].BU_KM > 10 && row.CHILOMETRAGGIO > this.CpList.TARGA[row.TARGA].BU_KM && (row.TARGA).substring(0, 3) != "CIS") { //row.CHILOMETRAGGIO > 10 && this.CpList.TARGA[row.TARGA].BU_KM > 10
+
+                                        //this.CpList.TARGA[row.TARGA].KM_TOT += row.CHILOMETRAGGIO - this.CpList.TARGA[row.TARGA].BU_KM;
+                                        this.CpList.TOTALE.KM_VEI += row.CHILOMETRAGGIO - this.CpList.TARGA[row.TARGA].BU_KM;
+
+                                        this.CpList.TARGA[row.TARGA].BU_KM = row.CHILOMETRAGGIO;
+                                    }
+
+                                    if (row.CHILOMETRAGGIO < 10 || row.CHILOMETRAGGIO < this.CpList.TARGA[row.TARGA].BU_KM) {
+                                        this.CpList.TARGA[row.TARGA].KM_ERROR = true;
+                                    }
+
+                                    let Data = row.DATA.substring(0, 10).split('-');
+
+                                    //const month = new Date(Data[2], Data[1] - 1, Data[0]).toLocaleString('default', { month: 'long' });
+
+                                    this.CpList.TARGA[row.TARGA].TRANSAZIONI.push({
+                                        //PRODOTTO: row["DESCRIZIONE PRODOTTO 1"],
+                                        //PREZZO: row["PREZZO UNITARIO"],
+                                        KM: row.CHILOMETRAGGIO,
+                                        L: row.QUANTITÀ,
+                                        //IMPORTO: row.IMPORTO,
+                                        DATA: new Date(Data[2], Data[1] - 1, Data[0]),
+                                        //LOCALITÀ: row.LOCALITÀ,
+                                        //AUTISTA: row["CODICE AUTISTA"],
+                                    });
+                                }
+                            });
+
+                            console.log(this.CpList);
+
+                            this.CpTot = [
+                                ["Cisterna", 0, (this.CpList.TOTALE.L_CIS).toFixed(2)],
+                                ["Veicolo", (this.CpList.TOTALE.KM_VEI).toFixed(0), (this.CpList.TOTALE.L_VEI).toFixed(2)],
+                            ];
+                            //console.log(this.CpTot);
+
+                            /*this.CpTable = [];
+                            Object.keys(this.CpList.TARGA).forEach(element => {
+                                this.CpTable.push([element, this.CpList.TARGA[element].DATA[0], this.CpList.TARGA[element].DATA[this.CpList.TARGA[element].DATA.length - 1], this.CpList.TARGA[element].KM_TOT.toFixed(0), this.CpList.TARGA[element].L_TOT.toFixed(2), this.CpList.TARGA[element].KM_ERROR, this.CpList.TARGA[element].IMPORTO_TOT.toFixed(2)]);
+                            });*/
+                            //console.log(this.CpTable);
+
+                            //console.log(loadFileData);
+
+                            let tot = 0;
+                            Object.keys(this.CpList.TARGA).forEach(targa => {
+                                let EstrattoTarga = this.CpList.TARGA[targa].TRANSAZIONI.sort(function (a, b) {
+                                    return a.DATA - b.DATA;
+                                });
+                                
+                                //console.log(EstrattoTarga);
+
+                                let min = 0;
+                                for (let i = 0; i < EstrattoTarga.length - 1; i++) {
+                                    if ((targa).substring(0, 3) != "CIS") {
+                                        if (EstrattoTarga[i].KM > min) {
+                                            min = EstrattoTarga[i].KM;
+                                        }
+                                        if (EstrattoTarga[i + 1].KM > min && min > 5) {
+                                            //console.log(i + ": " + EstrattoTarga[i + 1].KM + " + " + min + " = " + (EstrattoTarga[i + 1].KM - min));
+
+                                            this.CpList.TARGA[targa].KM_TOT += EstrattoTarga[i + 1].KM - min;
+                                            this.CpList.TARGA[targa].KM_TOT_M[EstrattoTarga[i + 1].DATA.toLocaleString('default', { month: 'long' })] += EstrattoTarga[i + 1].KM - min;
+                                            this.CpList.TOTALE.KM_TOT_M[EstrattoTarga[i + 1].DATA.toLocaleString('default', { month: 'long' })] += EstrattoTarga[i + 1].KM - min;
+                                            tot += EstrattoTarga[i + 1].KM - min;
+                                       
+                                        }
+                                    }
+                                }
+                                console.log(targa + ": " + this.CpList.TARGA[targa].KM_TOT);
+                            });
+
+                            console.log(tot);
+
+
+                            this.page_loadFile = false;
+                            this.page_CpList = true;
+
+                            this.page_CpList_loader = false;
+                            this.btnLoad_isDisabled = false;
+
+                            worker.terminate();
                         }
-
-                        if (row["PREZZO UNITARIO"] > 1 && row["PREZZO UNITARIO"] < 10 && row.QUANTITÀ != 0) {//&& row.CHILOMETRAGGIO > 10 //row.QUANTITÀ != 0 PREZZO UNITARIO
-
-                            this.CpList.TARGA[row.TARGA].KM.push(row.CHILOMETRAGGIO);
-
-                            this.CpList.TARGA[row.TARGA].DATA.push(row.DATA.substring(0, 10));
-                            this.CpList.TARGA[row.TARGA].LOCALITÀ.push(row.LOCALITÀ);
-                            this.CpList.TARGA[row.TARGA].AUTISTA.push(row["CODICE AUTISTA"]);
-
-                            this.CpList.TARGA[row.TARGA].L.push(row.QUANTITÀ);
-                            this.CpList.TARGA[row.TARGA].L_TOT += row.QUANTITÀ;
-
-                            this.CpList.TARGA[row.TARGA].IMPORTO.push(row.IMPORTO);
-                            this.CpList.TARGA[row.TARGA].IMPORTO_TOT += row.IMPORTO;
-
-                            if ((row.TARGA).substring(0, 3) == "CIS") {
-                                this.CpList.TOTALE.L_CIS += row.QUANTITÀ;
-                            } else {
-                                this.CpList.TOTALE.L_VEI += row.QUANTITÀ;
-                            }
-
-                            if ( row.CHILOMETRAGGIO > 10 && this.CpList.TARGA[row.TARGA].BU_KM > 10 && row.CHILOMETRAGGIO > this.CpList.TARGA[row.TARGA].BU_KM && (row.TARGA).substring(0, 3) != "CIS") { //row.CHILOMETRAGGIO > 10 && this.CpList.TARGA[row.TARGA].BU_KM > 10
-
-                                this.CpList.TARGA[row.TARGA].KM_TOT += row.CHILOMETRAGGIO - this.CpList.TARGA[row.TARGA].BU_KM;
-                                this.CpList.TOTALE.KM_VEI += row.CHILOMETRAGGIO - this.CpList.TARGA[row.TARGA].BU_KM;
-
-                                this.CpList.TARGA[row.TARGA].BU_KM = row.CHILOMETRAGGIO;
-                            } 
-
-                            if (row.CHILOMETRAGGIO < 10 || row.CHILOMETRAGGIO < this.CpList.TARGA[row.TARGA].BU_KM) {
-                                this.CpList.TARGA[row.TARGA].KM_ERROR = true;
-                            }
-
-                        }
-                    });
-
-                    console.log(this.CpList);
-
-                    this.CpTot = [
-                        ["Cisterna", 0, (this.CpList.TOTALE.L_CIS).toFixed(2)],
-                        ["Veicolo", (this.CpList.TOTALE.KM_VEI).toFixed(0), (this.CpList.TOTALE.L_VEI).toFixed(2)],
-                    ];
-                    console.log(this.CpTot);
-
-                    this.CpTable = [];
-                    Object.keys(this.CpList.TARGA).forEach(element => {
-                        this.CpTable.push([element, this.CpList.TARGA[element].DATA[0], this.CpList.TARGA[element].DATA[this.CpList.TARGA[element].DATA.length - 1], this.CpList.TARGA[element].KM_TOT.toFixed(0), this.CpList.TARGA[element].L_TOT.toFixed(2), this.CpList.TARGA[element].KM_ERROR, this.CpList.TARGA[element].IMPORTO_TOT.toFixed(2)]);
-                    });
-                    console.log(this.CpTable);
-
-                    this.page_loadFile = false;
-                    this.page_CpList = true;
-
-                    this.page_CpList_loader = false;
-                    this.btnLoad_isDisabled = false;
-
-                    worker.terminate();
+                    }
+                    readFile(index + 1);
                 }
+                readFile(0);
             }
         },
         backLCList() {
